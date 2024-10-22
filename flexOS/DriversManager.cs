480c55865459af0556b9817552b9370f00d438f1
Andro_Dev <87223939+AndroMaster436@users.noreply.github.com>
2024-10-22 16:18:16 +0000

    add ping command

---
+++ flexOS/DriversManager.cs
@@ -0,0 +1,18 @@
+using System.Net.Sockets;
+using Cosmos.HAL;
+using Cosmos.System.Network.Config;
+using Cosmos.System.Network.IPv4;
+
+namespace FlexOS
+{
+    public class DriversManager
+    {
+        private NetworkDevice networkDevice;
+
+        public static void Init()
+        {
+            networkDevice = NetworkDevice.GetDeviceByName("eth0")
+            IPConfig.Enable(networkDevice, new Address(192, 168, 1, 64), new Address(255, 255, 255, 0), new Address(192, 128, 1, 254));
+        }
+    }
+}
--- flexOS/Kernel.cs
+++ flexOS/Kernel.cs
@@ -1,5 +1,10 @@
 using System;
 using Sys = Cosmos.System;
+using Cosmos.System.Network.IPv4.UDP.DNS;
+using Cosmos.System.Network.IPv4;
+using System.Runtime.InteropServices;
+using System.Net.Sockets;
+using System.Text;
 
 namespace FlexOS
 {
@@ -7,17 +12,18 @@ namespace FlexOS
     {
         protected override void BeforeRun()
         {
+            DriversManager.Init();
             Console.Clear();
-            Console.WriteLine(@"""
-               __ _            ____   _____ 
+            Console.WriteLine(@"
+   __ _            ____   _____ 
   / _| |          / __ \ / ____|
  | |_| | _____  _| |  | | (___  
  |  _| |/ _ \ \/ / |  | |\___ \ 
  | | | |  __/>  <| |__| |____) |
  |_| |_|\___/_/\_\\____/|_____/ 
                                 
-                                """);
-            Console.WriteLine("Welcome to flexOS\nType help for command list");
+                                ");
+            Console.WriteLine("Welcome to flexOS\nType help for command list\n");
         }
 
         protected override void Run()
@@ -25,9 +31,22 @@ namespace FlexOS
             while (true)
             {
                 Console.Write("root@flexOS> ");
-                var cmd = Console.ReadLine();
-                switch (cmd)
+                var command = Console.ReadLine();
+                string[] cmd = command.Split(" ").ToLower();
+                switch (cmd[0])
                 {
+                    case "ping":
+                        if (cmd.Length == 1) Console.WriteLine("Usage: ping [SITE]");
+                        else
+                        {
+                            using (var dnsClient = new DnsClient())
+                            {
+                                dnsClient.Connect(new Address(8, 8, 8, 8));
+                                dnsClient.SendAsk(cmd[1]);
+                                Console.WriteLine($"Pinged! IP: {dnsClient.Receive()}");
+                            }
+                        }
+                        break;
                     default:
                         Console.WriteLine($"Incorrect command: {cmd}");
                         break;
